360ccb6516bddaafae618959af19237fddfc75ca
marcu <marcusdasilva19@outlook.com>
2025-02-15 23:34:59 -0300

    Atualização na porta do localhost para testes.

--- src/main/java/br/cefetmg/gestaodechamadas/controller/HomeController.java
+++ src/main/java/br/cefetmg/gestaodechamadas/controller/HomeController.java
@@ -1,11 +1,13 @@
 package br.cefetmg.gestaodechamadas.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/")
+@CrossOrigin(origins = "http://localhost:5173")
 public class HomeController {
 
     @GetMapping("")
